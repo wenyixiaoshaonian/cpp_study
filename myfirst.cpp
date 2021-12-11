8d6973aa6105ef4f70c4f774e8974e2bc0d6a71d
caoboxi <541190157@qq.com>
2021-12-11 13:18:16 +0800

    Description：添加第一个demo place(home/move):home author:cbx

---
+++ myfirst.cpp
@@ -0,0 +1,11 @@
+// my first cpp
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "my first cpp....";
+    cout << endl;
+    cout << "hello world" << endl;
+    return 0;
+}
